ba01d94c553d97b24b906fdc8e09890938e9b061
looeon <mustsum123@gmail.com>
2024-04-22 10:45:13 +0900

    04221045

--- tomcat.sh
+++ tomcat.sh
@@ -1,4 +1,4 @@
 export CLASSPATH=$CLASSPATH:/root/apache-tomcat-10.1.20/lib/mysql-connector-j.jar
 export JAVA_HOME=/usr/lib/jvm/java-17-openjdk
 export CATALINA_HOME=/root/apache-tomcat-10.1.20
-export CATALINA_BASE=\$CATALINA_HOME
+export CATALINA_BASE=$CATALINA_HOME
