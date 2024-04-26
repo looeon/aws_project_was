00c47b47a1c08011befc95c001817c7f08a5246e
looeon <mustsum123@gmail.com>
2024-04-26 15:46:24 +0900

    04261546

--- dbconnector.sh
+++ dbconnector.sh
@@ -1,3 +1,5 @@
+sudo wget https://github.com/looeon/aws_project_was/raw/main/mysql-connector-j-8.3.0-1.el9.noarch.rpm -O /root/mysql-connector-j-8.3.0-1.el9.noarch.rpm
+sudo rpm2cpio /root/mysql-connector-j-8.3.0-1.el9.noarch.rpm > /root/mysql-connector-j.cpio
 sudo cd /root && sudo cpio -idmv < /root/mysql-connector-j.cpio
 sudo cp /root/usr/share/java/mysql-connector-j.jar /root/apache-tomcat-10.1.20/lib/mysql-connector-j.jar
 sudo chmod 640 /root/apache-tomcat-10.1.20/lib/mysql-connector-j.jar
